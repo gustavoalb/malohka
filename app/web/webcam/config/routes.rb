0f943dd8d88f62e7f2b896650697385385f9e1da
Gustavo Albuquerque <alucard.ap@gmail.com>
2015-06-06 16:42:38 -0300

    cooreção de Rota

--- app/web/webcam/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  resources :photos, :only => [:index, :show, :new, :create] do
-    post 'upload', :on => :collection
-  end
-
-  root :to => redirect("/photos")
-end
