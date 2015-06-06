744ae18a3d293754cd6f7cd8226a0321b86d78f1
Gustavo Albuquerque <alucard.ap@gmail.com>
2015-06-06 15:42:31 -0300

    cooreção de Rota

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   end
 
   resources :noticias do
-    put "alterar_status/:noticia_id/:status"#=>'noticias#alterar_status', as: :alterar_status
+    member do
+      put "alterar_status/:status"=>"noticias#alterar_status"
+    end
   end
 
   ##put "atualizar_status/:noticia_id/:status"=>'noticias#atualizar_status', as: :atualizar_status
